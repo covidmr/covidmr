82e006d88793b5d42c60073c7794b4ac771e5455
T0X <T0X@T0XMac.local>
2020-04-03 06:11:49 +0200

    Updates

---
+++ precache-manifest.10026ce8437ac849c1a49a157033d17a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693df8ac2d8d7db509860adf5887d642",
+    "url": "/covidmr/index.html"
+  },
+  {
+    "revision": "2de52f9e9af18046904d",
+    "url": "/covidmr/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "fd5eb678bb03e5d2041f",
+    "url": "/covidmr/static/css/main.38a5f2cb.chunk.css"
+  },
+  {
+    "revision": "2de52f9e9af18046904d",
+    "url": "/covidmr/static/js/2.801b3e0d.chunk.js"
+  },
+  {
+    "revision": "f7f8dfb2e4bd7743c3a1f5578d54e932",
+    "url": "/covidmr/static/js/2.801b3e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5eb678bb03e5d2041f",
+    "url": "/covidmr/static/js/main.10e11b17.chunk.js"
+  },
+  {
+    "revision": "1c40e2aa83a244c1ad3e",
+    "url": "/covidmr/static/js/runtime-main.29f54fd4.js"
+  },
+  {
+    "revision": "f1452b6e6773ed885eb00b801deb0a8c",
+    "url": "/covidmr/static/media/corona.f1452b6e.jpg"
+  },
+  {
+    "revision": "e122ebbf800c2140ae889086aa2f8aaf",
+    "url": "/covidmr/static/media/coude.e122ebbf.png"
+  },
+  {
+    "revision": "99e77e3c86a03b2560f2a37f4bd90850",
+    "url": "/covidmr/static/media/flag-mauritania.99e77e3c.png"
+  },
+  {
+    "revision": "1c2c43ef5272d3bd8c4816fbbacc8746",
+    "url": "/covidmr/static/media/logo.1c2c43ef.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covidmr/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "15917b59ffe193c16a2335946a627d00",
+    "url": "/covidmr/static/media/maison.15917b59.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2562fbd64a5aeef47231e4e7f4b49104.js"
+  "/covidmr/precache-manifest.10026ce8437ac849c1a49a157033d17a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covidmr/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
